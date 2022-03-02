1503556de2da31f69071a39784a0404bf8614da2
Jonah Milner <jonahmilner86@gmail.com>
2022-03-01 21:07:56 -0700

    added a function

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,33 @@
-let apiUrl = ""
+// let apiUrl = `http://api.openweathermap.org/data/2.5/forecast?id=524901&appid=0b106405e1fea6fced972a3d3945745f`
+// 0b106405e1fea6fced972a3d3945745f
+// console.log(apiUrl);
+
+
+// const getData = () => {
+//     fetch(apiUrl).then(function(response){
+//         if (response.ok) {
+//             response.json().then(function(data){
+//                 console.log(data);
+//             })
+//         }
+//         else {
+//             alert("Error, weather API not found")
+//         }
+//     })
+//     .catch(function(error){
+//        alert("Unable to connect to weather API")     
+//     });
+// };
+
+
+
+
+
+var apiUrl = `https://humble-bundle.p.rapidapi.com/get-bundles`
 
 console.log(apiUrl);
+
+
+
+
+
