0332a0be62a68461545d9b8b350acc5f65d96e01
Jonah Milner <jonahmilner86@gmail.com>
2022-04-23 14:59:53 -0600

    added a lot of requirements to html file

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,33 +1,22 @@
-// let apiUrl = `http://api.openweathermap.org/data/2.5/forecast?id=524901&appid=0b106405e1fea6fced972a3d3945745f`
-// 0b106405e1fea6fced972a3d3945745f
-// console.log(apiUrl);
-
-
-// const getData = () => {
-//     fetch(apiUrl).then(function(response){
-//         if (response.ok) {
-//             response.json().then(function(data){
-//                 console.log(data);
-//             })
-//         }
-//         else {
-//             alert("Error, weather API not found")
-//         }
-//     })
-//     .catch(function(error){
-//        alert("Unable to connect to weather API")     
-//     });
-// };
-
-
-
-
-
-var apiUrl = `https://humble-bundle.p.rapidapi.com/get-bundles`
-
+let apiUrl = `http://api.openweathermap.org/data/2.5/forecast?id=524901&appid=0b106405e1fea6fced972a3d3945745f`
 console.log(apiUrl);
 
+// API key 0b106405e1fea6fced972a3d3945745f
 
 
-
-
+let currentWeather = `https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid=0b106405e1fea6fced972a3d3945745f`
+const getCurrentWeather = () => {
+    fetch(currentWeather).then(function(response){
+        if (response.ok) {
+            response.json().then(function(data){
+                console.log(data);
+            })
+        }
+        else {
+            alert("Error, weather API not found")
+        }
+    })
+    .catch(function(error){
+       alert("Unable to connect to weather API")     
+    });
+};
